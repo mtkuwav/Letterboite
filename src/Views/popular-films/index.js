700b3a8195157a3cce9afbcc1b08f40a31cd820a
mtkuwav <math.chaulab@gmail.com>
2025-01-23 15:49:46 +0100

    feat(translations): enhanced website multilanguage support with changing HTML text and date based on the language choosen

--- src/Views/popular-films/index.js
+++ src/Views/popular-films/index.js
@@ -1,3 +1,22 @@
+const translations = {
+  'fr-FR': {
+    releaseDate: 'Date de sortie',
+    viewDetails: 'Voir plus'
+  },
+  'en-US': {
+    releaseDate: 'Release date',
+    viewDetails: 'See more'
+  },
+  'de-DE': {
+    releaseDate: 'Veröffentlichungsdatum',
+    viewDetails: 'Mehr erfahren'
+  },
+  'es-ES': {
+    releaseDate: 'Fecha de lanzamiento',
+    viewDetails: 'Ver más'
+  }
+};
+
 const film = (data) => {
   const {
     title,
@@ -6,17 +25,20 @@ const film = (data) => {
     id
   } = data;
 
+  const currentLang = localStorage.getItem('language') || 'fr-FR';
+  const text = translations[currentLang];
+
   return (`
     <div class="col-3 mt-4" id="Films">
       <div class="card">
         <img src="https://image.tmdb.org/t/p/w500/${posterPath}" class="card-img-top" alt="poster">
         <div class="card-body text-center">
           <h5 class="card-title">${title}</h5>
-          <p>Date de sortie: ${new Date(releaseDate).toLocaleDateString('fr')}</p>
+          <p>${text.releaseDate} : ${new Date(releaseDate).toLocaleDateString(localStorage.getItem('language'))}</p>
           <a
             href="/film?id=${id}"
             class="btn btn-primary"
-          >Voir plus</a>
+          >${text.viewDetails}</a>
         </div>
       </div>
     </div>
