983db3047291b5284167804794ef5b8c9a5f1810
mtkuwav <math.chaulab@gmail.com>
2025-01-23 14:43:57 +0100

    feat(navigation): update navbar brand link to point to popular films page

--- src/Views/popular-films/nav.js
+++ src/Views/popular-films/nav.js
@@ -16,7 +16,7 @@ export default () => {
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-        <a class="navbar-brand text-light" href="#">Letterboîte</a>
+        <a class="navbar-brand text-light" href="/popular-films">Letterboîte</a>
         <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         </form>
